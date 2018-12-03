98adf327959b4b19a19bebdfc482fab289f9f9f3
Bakal Web <adambakaldev@gmail.com>
2018-12-03 11:21:27 +0000

    Installed moment js and added to bookings component

--- apps/booking/src/app/features/dashboard/dashboard.module.ts
+++ apps/booking/src/app/features/dashboard/dashboard.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DashboardComponent } from './containers/dashboard/dashboard.component';
 import { MaterialModule } from '@core/modules/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { DashboardComponent } from './containers/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
